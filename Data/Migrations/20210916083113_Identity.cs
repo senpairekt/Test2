bccc4930d2c21a81bab778756ae73656a13fb0e5
Jan M <feralsvk456@gmail.com>
2021-09-19 20:47:32 +0200

    Add project files.

---
+++ Data/Migrations/20210916083113_Identity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2021, 9, 16, 8, 31, 13, 240, DateTimeKind.Utc).AddTicks(7534));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2021, 9, 16, 8, 28, 23, 157, DateTimeKind.Utc).AddTicks(4175));
+        }
+    }
+}
